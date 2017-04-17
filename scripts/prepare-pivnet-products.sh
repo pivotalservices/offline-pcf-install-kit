4b9c9e0e728e3d735304000281d9f0f8ab528016
Matthew Campbell <mjc4648@gmail.com>
2017-04-17 11:41:48 -0400

    working towards a minio preparation pipeline

--- scripts/prepare-pivnet-products.sh
+++ scripts/prepare-pivnet-products.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 set -ex
-ls -la
+
+PIVNET_CLI=`find ./pivnet-cli -name "*linux-amd64*"`
+chmod +x $PIVNET_CLI
+
+JQ=`find ./jq -name "*linux64*"`
+chmod +x $JQ
+
+echo "Downloading products from pivnet..."
+$PIVNET_CLI login --api-token="$PIVNET_TOKEN"
+$PIVNET_CLI products --format=json | $JQ
+
+
+
+# URL="http://myriak.server:8098/buckets/mybucket/index/index_int/100/200/"
+# curl -Ss  "$URL" | jq -r '.keys[] | @uri' |\
+# while read key
+# do
+#   curl "http://myriak/buckets/mybucket/keys/$key"
+# done | jq '.[] | {id}'
+
+
+
+# docker run minio:...
+#
+# fill the s3 bucket
+#
+# export the docker container
+#
+# output the image as our task artifact
